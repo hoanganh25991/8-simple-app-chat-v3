04b4d25a82e52682217dd0978929b524d16b75de
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-03-11 10:42:35 +0700

    handle-messages.js, client get list-active-users, send/receive msg, this socket belong to a room, userA

--- public/javascripts/handle-message.js
+++ public/javascripts/handle-message.js
@@ -1,64 +1,53 @@
 var socket;
 window.onload = function(){
-    /**
-     * A SEND MSG TO B
-     * A LISTEN MSG FROM B
-     * >>>>TASK MEANS>>>>>>
-     * [1]. send msg from A to server
-     * when form submit msg, means A want to send msg to B
-     * server help us delivery this msg to B
-     * [2]. receive msg from server
-     * server has filtered which msg from B to A
-     */
     //get info
-    var userA= $("#userA").val();
-    var userB = $("#userB").val();
+    var userA_sender= $("#userA_sender").val();
     //get socket from namespace
     var host = "http://localhost:3000/";
-    socket = io(host + userB);
-    /**
-     * 1. send msg from A to server, when form submit
-     */
+    var namespaceDefault = "/";
+    socket = io(host + namespaceDefault);
+    //when userA click on [chat] to chat with others,get list-active-users for him
+    socket.emit("list-active-users", "server give me list active users");
+    //listen to this list from server
+    socket.on("list-active-users", function(list_active_users){
+        //show list
+    });
+    //update this list when new active user come
+    socket.on("new-active-user", function(new_active_user){
+        //update list
+    });
+    //when userA click on [userB] to chat with him, get userB
+    var userB_receiver = $("#userB_receiver").val();
     $("#form-message").submit(function(){
         //get msg A to B
         var input_msg = $("#input-message");
         var msgAtoB = input_msg.val();
         //clear input message
         input_msg.val("");
-        //append to list-messages chat
+        //update conversation (A,B)
         var div = $("<div>").attr("class", "msg-right").text(msgAtoB);
         $("#list-messages").append($("<li>").append(div));
         //build PersonalMessage-object, from A to B
         var msgFromAObject = {
             msg: msgAtoB,
             createAt: new Date().toString(),
-            from: userA,
-            to: userB
+            from: userA_sender,
+            to: userB_receiver
         };
-        //send to server, DONE
-        //under namespaceUserB, A send msg to server, then server search for B, send to B
-        socket.emit(userB, msgFromAObject);
+        //send to server, server will delivery it
+        //on client-side, we can not see others client
+        //through server, send msg to B
+        socket.emit("clientChatMsg", msgFromAObject);
         //cancel submit, we MUST do this, if not, page reload
         return false;
     });
     /**
      * 2. receive msg from server
      */
-    socket.on(userA, function(msgFromXObject){
-        //server not filter msg from X, Y, Z to A
-        //vi khi B o mot noi khac, da muon namespaceUserA de emit(msg), khi emit dau co find A, emit all members in namespaceUserA
-        //log msg info
+    socket.on("clientChatMsg", function(msgFromXObject){
         console.log("msgFromBOjebct:", msgFromXObject);
-        //if check msg from B
-        if(msgFromXObject.from == userB){
-            //append to list-messages, msg from B to A
-            var div = $("<div>").attr("class", "msg-left").text(msgFromXObject.msg);
-            $("#list-messages").append($("<li>").append(div));
-        }
-    });
-
-    var socketDefault = io();
-    socketDefault.on("time", function(msg){
-        console.log(msg);
+        //update conversation (A,B)
+        var div = $("<div>").attr("class", "msg-left").text(msgFromXObject.msg);
+        $("#list-messages").append($("<li>").append(div));
     });
 };
